12183f3a259f1bfa8c2aab11c48278232cc9ddde
Gennady <46938551+gkaluga@users.noreply.github.com>
2024-08-31 00:36:29 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+number_of_completed_HA = 12
+number_of_hours_spent = 1.5
+course_title = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_HA
+print("Курс:",course_title,", всего задач:",number_of_completed_HA,", затрачено часов:",number_of_hours_spent,", среднее время выполнения",time_for_one_task,"часа.")
