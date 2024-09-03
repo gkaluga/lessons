207c5104317fb4db4f50ba460b1f8bc4eff5f472
Gennady <kaluga64@mail.ru>
2024-09-03 19:46:18 +0300

    03.09

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 3, "string", True
+mutable_list = [45, 77, "string",False]
+mutable_list[2] = "Modified"
+mutable_list[1] = True
+print("Immutable tuple:", immutable_var)
+print("Mutable list:", mutable_list)
