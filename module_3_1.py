30b09432319d6952f1bd3c2c5ae6739c38faf728
Gennady <kaluga64@mail.ru>
2024-09-14 12:24:03 +0300

    09.09

--- module_3_1.py
+++ module_3_1.py
@@ -2,6 +2,9 @@ def count_calls():
     global calls
     calls += 1
 
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
 
 def is_contains(string, list_to_search):
     count_calls()
@@ -9,10 +12,6 @@ def is_contains(string, list_to_search):
         if string.lower() == str.lower():
             return True #  , f'{string} ~ {str}'
     return False
-
-def string_info(string):
-    count_calls()
-    return len(string), string.upper(), string.lower()
 
 
 calls = 0
