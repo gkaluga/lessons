a153ebaf2103396737f008c4686cd55b75ed20b9
Gennady <kaluga64@mail.ru>
2024-09-21 19:22:45 +0300

    21.09.2024

---
+++ module_3_hard.py
@@ -0,0 +1,24 @@
+def calculate_structure_sum(data_):
+    if isinstance(data_, (list, tuple, set)):
+        result = 0
+        for k in data_:
+            result += calculate_structure_sum(k)
+        return result
+    elif isinstance(data_, dict):
+        return calculate_structure_sum(tuple(data_.items()))
+    elif isinstance(data_, (int, float)):
+        return data_
+    else:
+        return len(data_)
+
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
